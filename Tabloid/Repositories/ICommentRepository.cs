c40af483012b24773391d95bee363e461110e7dc
Faith Magras <bdanielle092@gmail.com>
2020-10-07 11:59:21 -0500

    working on adding files

---
+++ Tabloid/Repositories/ICommentRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+        void AddComment(Comment comment);
+        void DeleteComment(int id);
+        Comment GetCommentById(int id);
+        System.Collections.Generic.List<Comment> GetCommentByUserProfileId(int userProfileId);
+        System.Collections.Generic.List<Comment> GetCommentsByPostId(int id);
+        void UpdateComment(Comment comment);
+    }
+}
