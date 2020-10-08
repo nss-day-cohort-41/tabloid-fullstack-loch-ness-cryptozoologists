6701e3df4cf200452e4c58a87aec38aba314c24b
Rob Mixon <mixonroberth@gmail.com>
2020-10-08 14:02:50 -0500

    fixed merge issues

--- Tabloid/Models/Category.cs
+++ Tabloid/Models/Category.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,21 +5,11 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace Tabloid.Models
-=======
-﻿namespace Tabloid.Models
->>>>>>> master
 {
     public class Category
     {
         public int Id { get; set; }
-<<<<<<< HEAD
         [Required]
         public string Name { get; set; }
     }
 }
-=======
-        public string Name { get; set; }
-
-    }
-}
->>>>>>> master
--- Tabloid/Startup.cs
+++ Tabloid/Startup.cs
@@ -22,11 +22,8 @@ namespace Tabloid
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IUserProfileRepository, UserProfileRepository>();
-<<<<<<< HEAD
             services.AddTransient<ICategoryRepository, CategoryRepository>();
-=======
             services.AddTransient<IPostRepository, PostRepository>();
->>>>>>> master
 
             var firebaseProjectId = Configuration.GetValue<string>("FirebaseProjectId");
             var googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
