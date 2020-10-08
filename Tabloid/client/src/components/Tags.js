b2c4dead759d28d4717eb938af9340334e214db6
drewharper <druharper@gmail.com>
2020-10-08 16:21:38 -0500

    Pretty tags

--- Tabloid/client/src/components/Tags.js
+++ Tabloid/client/src/components/Tags.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { TagContext } from "../providers/TagProvider";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import { Badge } from "reactstrap";
 const Tags = () => {
     const { tags, getAllTags } = useContext(TagContext);
 
@@ -11,15 +11,12 @@ const Tags = () => {
     return (
         <div>
             <h1>Tags</h1>
-            {tags.map((tags) => (
-
-                <div key={tags.id}>
-                    <ListGroup>
-                        <ListGroupItem>{tags.name}</ListGroupItem>
-                    </ListGroup>
-
-                </div>
-            ))}
+            <div className="d-flex flex-wrap" key={tags.id}>
+                {tags.map((tags) => (
+                    <Badge color="primary" pill className="dh-badge m-1 pl-3 pr-3">{tags.name} <span className="xOut">&#10005;</span>
+                    </Badge>
+                ))}
+            </div>
         </div>
     );
 };
