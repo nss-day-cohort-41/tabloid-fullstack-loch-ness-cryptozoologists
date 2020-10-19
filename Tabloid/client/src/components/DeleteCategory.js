47e19028bb03ffd7084b5f41f2550ab55f74b2f2
Faith Magras <bdanielle092@gmail.com>
2020-10-19 15:45:37 -0500

    working on delete

--- Tabloid/client/src/components/DeleteCategory.js
+++ Tabloid/client/src/components/DeleteCategory.js
@@ -1,41 +1,47 @@
 
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { CategoryContext } from "../providers/CategoryProvider";
 import { Card, CardBody, Button } from "reactstrap";
-import { UserProfileContext } from "../providers/UserProfileProvider";
+
 
 const DeleteCategory = () => {
 
-
     const { id } = useParams();
-
     const history = useHistory();
-    const { categories, deleteCategory, getCategoryById } = useContext(CategoryContext);
-    const { activeUser } = useContext(UserProfileContext);
+    const { category, deleteCategory, getSingleCategory } = useContext(CategoryContext);
+    const [categoryText, setCategoryText] = useState({})
 
-
+    const handleDelete = (evt) => {
+        evt.preventDefault();
+        deleteCategory(categoryText)
+            .then(() => history.push("/category"))
+            .catch((err) => alert(`An error ocurred: ${err.message}`));
+    }
     useEffect(() => {
-        getCategoryById(id);
+        getSingleCategory(id);
     }, [])
 
 
-    const deleteACategory = () => {
-        deleteCategory(id).then(history.goBack())
-    }
+
+    useEffect(() => {
+        setCategoryText(category);
+
+    }, [category]);
 
     return (
         <>
             <div>
 
                 <h3>Are you sure you want to delete this category? </h3>
+                <p>Any Post using Category <i>{category.name}</i>will change to other</p>
 
-                <Button block className="deleteCategoryButton" type="button" color="secondary" onClick={deleteACategory}>
-                    {'Delete Category'}
+                <Button type="button" color="secondary" onClick={handleDelete}>
+                    Delete
                 </Button>
 
-                <Button block className="returnToListButton" type="button" color="secondary" onClick={() => history.goBack()}>
+                <Button type="button" id={category.id} color="secondary" onClick={() => history.goBack()}>
                     {'Cancel'}
                 </Button>
 
